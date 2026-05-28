71a26082e32dcc9dad584842e0a94215aa4784b0
unknown <vs5112136@gmail.com>
2026-05-28 18:05:13 +0530

    Initial commit(done till put ,patch,delete)

---
+++ schemas.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict,EmailStr, Field
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+    image_file: str | None = None
+
+
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    image_file:str | None
+    image_path: str
+
+class PostBase(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+    content: str = Field(min_length=1)
+  
+
+class PostCreate(PostBase):
+    user_id: int # temporary
+
+class PostUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=1, max_length=100)
+    content: str | None = Field(default=None, min_length=1)    
+
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    user_id:int
+    date_posted: datetime
+    author: UserResponse 
